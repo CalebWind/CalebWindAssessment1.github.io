feefe319145421b921477a30118c6427c199da71
arphonage <arphonage@gmail.com>
2019-10-14 09:51:31 +1300

    Stars added and json uploading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
-const alertUser = () => {
-    window.alert("Thank you for your message");
-}
+// $(function() {
+//     $.getJSON('json.json', function(data) {
+//         var tr;
 
-document.getElementById("submit").addEventListener("click", alertUser);
+//         for (var i = 0; i < data.length; i++) {
+
+//             tr = $('<tr/>');
+//             tr.append("<td>" + data[i][0] + "</td>");
+//             tr.append("<td>" + data[i][1] + "</td>");
+//             $('table').append(tr);
+//         }
+//     });
+// });
+
+
+$(function() {
+
+    var people = [];
+
+    $.getJSON('json.json', function(data) {
+        $.each(data.phone, function(i,f) {
+            var tblRow = "<tr>" + "<td>" + f.itemName + "</td>" + "<td>" + f.brand + "</td>" + "<td>" + f.cost + "</td>" + "<td>" + f.review + "</td>" + "</tr>"
+            $(tblRow).appendTo("#products tbody");
+        });
+    });
+});
